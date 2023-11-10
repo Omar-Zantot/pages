b150bd8d98f0944f693e9b8fcaa494947e0f3729
Omar-Zantout <omar82199@gmail.com>
2023-11-10 15:41:56 +0200

    working on directive

---
+++ src/app/class.directive.ts
@@ -0,0 +1,43 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+/**
+ * use appClass as property
+ * */
+@Directive({
+  selector: '[appClass]',
+})
+export class ClassDirective {
+  // @Input() backgroundColor!: string;
+  constructor(private element: ElementRef) {
+    // NEVER DO THIS !!!
+    // setTimeout(() => {
+    //   this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    // }, 50);
+  }
+  // @Input() set backgroundColor(color: string) {
+  //   this.element.nativeElement.style.backgroundColor = color;
+  // }
+  @Input('appClass') set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
+  }
+
+  @Input('appClass') set className(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      }
+    }
+  }
+}
+
+/**
+ **- Make our class directive apply a background color to
+ **  whatever element we apply the directive to.
+ * ? how we can actually access the element that we apply the directive to.
+ * ? how we can change some properties on it.
+ *
+ */
+
+/**
+ * const directive = new ClassDirective();
+ * directive.backgroundColor = 'red';
+ * */
